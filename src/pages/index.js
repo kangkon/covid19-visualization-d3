ca4937054abe7dec1e0b7eb70ab0fbfc44ef5405
Kangkon Saikia <kangkonsaikia@gmail.com>
2020-06-01 15:01:47 +0530

    bootstrap added

--- src/pages/index.js
+++ src/pages/index.js
@@ -254,7 +254,11 @@ export default withResizeDetector(({width}) => {
       <>
           <Helmet>
               <title>Covid19 Cases Visualization - Assam</title>
-              <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"/>
+              {
+                  /*
+                  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"/>
+                  */
+              }
 
           </Helmet>
           <div className={"container-fluid"}>
